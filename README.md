689d524686457f5be3e7a5d1a850c89a51b144bb
Lady3mlnm <lady3mlnm@gmail.com>
2018-11-08 08:34:50 +0300

    Expanding MyList with foreach, sort, zipWith, fold

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 This is code for learning project "MyList Collection" of the course "Rock the JVM! Scala and Functional Programming for Beginners" (by Daniel Ciocîrlan).
 https://www.udemy.com/rock-the-jvm-scala-for-beginners/
 
-This code simulates the code in the lectures, but contains small difference in formatting, some additional comments and tests.
+The project consists from only one file, but this file is constantly changing and supplemented. Test commands periodically are replaced. See commit history and release history for navigation.
+
+This code simulates the code in the lectures, but has differences in formatting, additional comments and tests.
 
 Development environment:
 * Language: Scala 2.12
--- src/MyList.scala
+++ src/MyList.scala
@@ -22,6 +22,12 @@ abstract class MyList[+A] {   // use covariance
 
   // concatenation
   def ++[B >: A](list: MyList[B]): MyList[B]
+
+  // hofs (high-order functions)
+  def foreach(f: A => Unit): Unit
+  def sort(compare: (A, A) => Int): MyList[A]
+  def zipWith[B, C](list: MyList[B], zip:(A, B) => C): MyList[C]
+  def fold[B](start: B) (operator: (B, A) => B): B
 }
 
 
@@ -37,6 +43,15 @@ case object Empty extends MyList[Nothing] {
   def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
 
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
+
+  // hofs
+  def foreach(f: Nothing => Unit): Unit = ()
+  def sort(compare: (Nothing, Nothing) => Int) = Empty
+  def zipWith[B, C](list: MyList[B], zip:(Nothing, B) => C) = {
+    if (!list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else Empty
+  }
+  def fold[B](start: B) (operator: (B, Nothing) => B): B = start
 }
 
 
@@ -87,6 +102,36 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
    */
   def flatMap[B](transformer: A => MyList[B]): MyList[B] =
     transformer(h) ++ t.flatMap(transformer)
+
+  // hofs
+  def foreach(f: A => Unit): Unit = {
+    f(h)
+    t.foreach(f)
+  }
+
+  def sort(compare: (A, A) => Int): MyList[A] = {              // use algorithm of insertion sort
+    def insert(x: A, sortedList: MyList[A]): MyList[A] =
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+
+    val sortedTail = t.sort(compare)
+    insert(h, sortedTail)
+  }
+
+  def zipWith[B, C](list: MyList[B], zip:(A, B) => C): MyList[C] =
+    if (list.isEmpty) throw new RuntimeException("Lists do not have the same length")
+    else new Cons(zip(h, list.head), t.zipWith(list.tail, zip))
+
+  /*
+    [1,2,3].fold(0)(+)
+    = [2,3].fold(1)(+)
+    = [3].fold(3)(+)
+    = [].fold(6)(+)
+    = 6
+   */
+  def fold[B](start: B) (operator: (B, A) => B): B =
+    t.fold(operator(start, h))(operator)
 }
 
 
@@ -112,15 +157,61 @@ object ListTest extends App {
   println( cloneListOfIntegers == listOfIntegers )              //> true
 
 
-  // my additional tests
+  // hofs tests
+  listOfIntegers.foreach(println)               // = listOfIntegers.foreach(x => println(x))
+  println(listOfIntegers.sort((x,y) => y - x))  // = println(listOfIntegers.sort(-_ + _))      //> [3 2 1]
+  println(anotherListOfIntegers.zipWith[String, String](listOfStrings, _ + "-" + _))           //> [4-Hello 5-Scala]
+
+  println(listOfIntegers.fold(0)(_ + _))                                                       //> 6
+
+
+  // additional tests for sort
   println
 
-  val listMyTest = (listOfIntegers ++ anotherListOfIntegers).map(elem => elem * 10)
-  println(listMyTest)                                                            //> [10 20 30 40 50]
-  println((listOfIntegers ++ anotherListOfIntegers).map(_ * 10))                 // syntax sugar
+  val t1: MyList[Int] = new Cons (7, new Cons(1, new Cons(5, new Cons(6, new Cons(2, new Cons(3, Empty))))))
+  println(t1.sort((x,y) => x - y))                                                             //> [1 2 3 5 6 7]
+  println(t1.sort((x,y) => y - x))                                                             //> [7 6 5 3 2 1]
 
-  println(listMyTest.flatMap(elem => new Cons(elem,new Cons(elem + 2, Empty))))  //> [10 12 20 22 30 32 40 42 50 52]
+
+  // other additional tests
+  try{
+    println(listOfIntegers.zipWith[String, String](listOfStrings, _ + "-" + _))       // java.lang.RuntimeException: Lists do not have the same length
+  } catch {
+    case e: RuntimeException => println(e)
+    case e: Exception => println("caught other exception")
+  }
+
+  println(new Cons(20, new Cons(30, Empty)).zipWith[String, String](listOfStrings, _ + "-" + _))  //> [20-Hello 30-Scala]
+
+  try{
+    println(new Cons(30, Empty).zipWith[String, String](listOfStrings, _ + "-" + _))  // java.lang.RuntimeException: Lists do not have the same length
+  } catch {
+    case e: RuntimeException => println(e)
+    case e: Exception => println("caught other exception")
+  }
+
+  println(listOfIntegers.fold(10)(_ + _))  //> 16
+  println(listOfIntegers.fold(0)(_ * _))   //> 0
+  println(listOfIntegers.fold(1)(_ * _))   //> 6
+  println(listOfIntegers.fold(2)(_ * _))   //> 12
 }
+
+
+
+/* TASK #7
+    Expand MyList
+      - foreach method A => Unit
+        [1,2,3].foreach(x => println(x))
+
+      - sort function ((A, A) => Int) => MyList
+        [1,2,3].sort((x, y) => y - x) => [3,2,1]
+
+      - zipWith (list, (A, A) => B) => MyList[B]
+        [1,2,3].zipWith([4,5,6], x * y) => [1 * 4, 2 * 5, 3 * 6] = [4,10,18]
+
+      - fold(start)(function) => a value
+        [1,2,3].fold(0)(x + y) = 0 + 1 + 2 + 3 = 6
+*/
 
 
 
